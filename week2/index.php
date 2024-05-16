f06591eba68f89d673185b55f0629abcd5003c6e
Ozan Lago-Sereflioglu <lago.ozan@gmail.com>
2024-05-15 20:35:35 -0400

    Uploaded file for lab 2

--- week2/index.php
+++ week2/index.php
@@ -1,1 +1,34 @@
-localhost/HTTP5225/WEEK2
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+  <!--PHP Command-->
+  <?php echo "<h1>Welcome!</h1><p> Let's learn! </p>"; ?>
+
+  <!--Unordered List-->
+    <?php echo
+  "<ul>
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+  </ul>"
+    ?>
+
+
+
+  <?php
+  $fName = 'Ozan';
+  $lName = 'Lago-Sereflioglu';
+
+  echo "<h3>" . "My name is " . $fName . " " . $lName . "</h3>";
+  ?>
+  
+  </body>
+</html>
